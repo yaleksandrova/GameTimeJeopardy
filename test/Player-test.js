726fb075d66ca8863a79265dc3de4635dc79e0b3
yaleksandrova <yaleksandrova@yahoo.com>
2019-07-12 19:47:36 -0600

    Set up Turn and Player classes

---
+++ test/Player-test.js
@@ -0,0 +1,31 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+
+import Player from '../src/Player';
+// import data from '../data/data.js';
+
+let player; 
+
+beforeEach(() => {
+  player = new Player('Moisey', 1);
+});
+
+describe('Player', () => {
+
+  it('should be a function', () => {
+    expect(Player).to.be.a('function');
+  });
+
+  it('should be an instance of Players', () => {
+    expect(player).to.be.an.instanceof(Player);
+  })
+
+  it('should have a name', function() {
+    expect(player.name).to.equal('Moisey');
+  });  
+
+  it('should have an id', function() {
+    expect(player.id).to.equal(1);
+  });  
+});
