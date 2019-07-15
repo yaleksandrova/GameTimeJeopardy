3d543847627a0b5aac41fc256f858c86bd225a36
yaleksandrova <yaleksandrova@yahoo.com>
2019-07-15 13:49:30 -0600

    Add spy on tests

--- test/Player-test.js
+++ test/Player-test.js
@@ -10,6 +10,8 @@ let player;
 beforeEach(() => {
   player = new Player('Moisey', 1);
 });
+// global.localStorage = {};
+// chai.spy.on(localStorage, ['setItem', 'getItem'], () => {});
 
 describe('Player', () => {
 
