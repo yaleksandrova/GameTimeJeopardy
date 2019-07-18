0ad0d29fabe8a1171449d383c1791ff3b144a36e
Alan Turing <freemasl.sf@gmail.com>
2019-07-18 16:31:34 -0600

    Fix methods to display matching categories and clues when a round is intstantiated

--- src/Clues.js
+++ src/Clues.js
@@ -3,24 +3,25 @@
 class Clues {
   constructor(data) {
     this.data = data;
+    this.randomCat
+    this.categories
   }
 
   shuffleCategories() {
     let categories = Object.entries(this.data.categories)
     let random = categories.sort(() => Math.random() - 0.5)
-    return random;
+    this.randomCat = random;
   }
 
   pickCategories() {
-    return this.shuffleCategories().splice(0, 4);
+    this.categories = this.randomCat.splice(0, 4)
   }
 
   findMatchingQuestions() {
     let allQuestions = [];
     let points = [100, 200, 300, 400];
-    let categories = this.pickCategories();
     let clues = this.data.clues;
-    let categoryIds = categories.map(category => category[1]);
+    let categoryIds = this.categories.map(category => category[1]);
     categoryIds.map(id => {
       points.forEach(point => {
         allQuestions.push(clues.filter(clue => clue.categoryId === id).find(clue => clue.pointValue === point));
