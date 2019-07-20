a2f222144d170a3136cfe2cab8613faf79b653d8
yaleksandrova <yaleksandrova@yahoo.com>
2019-07-19 18:39:16 -0600

    Update domUodates.js

--- src/DOMupdates.js
+++ src/DOMupdates.js
@@ -1,3 +1,4 @@
+
 import $ from 'jquery';
 import Clues from './Clues';
 import Data from '../src/data/data';
@@ -9,13 +10,11 @@ import Player from "./Player";
 let domUpdates = {
 
 
-  displayPlayersName(arrayOfPlayers) {
-    console.log(arrayOfPlayers)
-
-    arrayOfPlayers.forEach((person, index) => {
-      $(`.player${index}-name-display`).html(person.name);
-
+  displayPlayersName(data) {
+    const players = data.players.map(person => {
+      return person.name;
     })
+    return players;
   },
 
   //user interaction 
@@ -35,13 +34,13 @@ let domUpdates = {
     })
   },
 
-  displayCluesQuestions(clues) {
-    let findClues = clues.findMatchingQuestions();
-    clues.forEach((clue, index) => {
-      console.log('displayQ', clue.categoryId)
-      $(`#js-row-${index}`).html(clue.question);
-    })
-  },
+  // displayCluesQuestions(clues) {
+  //   let findClues = clues.findMatchingQuestions();
+  //   clues.forEach((clue, index) => {
+  //     console.log('displayQ', clue.categoryId)
+  //     $(`#js-row-${index}`).html(clue.question);
+  //   })
+  // },
 
   displayCategories(clues) {
     let findCat = clues.categories;
@@ -53,6 +52,8 @@ let domUpdates = {
   //check answer() have an event listener for submit answer button 
 
   //display final round(one category with one card only)
+
+  //display clues
 
   //display final round(one category with one card only)
 
