e8a7fe9147602527838b0fb7544689f3bf9d44a8
yaleksandrova <yaleksandrova@yahoo.com>
2019-07-19 18:38:07 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 let gameData;
 
+fetch('https://fe-apps.herokuapp.com/api/v1/gametime/1903/jeopardy/data')
+  .then(response => response.json())
+  .then(jepData => gameData = jepData.data)
+  .catch(error => console.log(error))
 
-import Clues from '../src/Clues';
 
-import Player from '../src/Player';
+import Clues from '../src/Clues'
+
+import Player from '../src/Player'
 
 import Game from '../src/game';
 
-import Round from '../src/round';
+import Round from '../src/round'
 
-import Turn from '../src/turn';
-
-
+// import Data from '../src/data/data'
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 import domUpdates from './domUpdates'
@@ -35,32 +38,6 @@ import './css/base.scss';
 
 $('#js-names-button').click(function(e) {
   e.preventDefault();
-
-
-
-  let displayJeopardy = document.querySelector("#display-jeopardy");
-  fetch('https://fe-apps.herokuapp.com/api/v1/gametime/1903/jeopardy/data')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(result) {
-      let clues = new Clues(result.data)
-      clues.shuffleCategories()
-      clues.pickCategories()
-      clues.findMatchingQuestions()
-      let players = [];
-      let player1 = new Player($('#js-input-player-1').val(), 1)
-      let player2 = new Player($('#js-input-player-2').val(), 2)
-      let player3 = new Player($('#js-input-player-3').val(), 3)
-      players.push(player1, player2, player3)
-      let game = new Game(clues, players)
-      let round = new Round(players, 1, clues.categories, clues.cards)
-      // let turn = new Turn(category, value, guess, player)
-      domUpdates.displayCluesIds(clues)
-      domUpdates.displayCategories(clues)   
-      domUpdates.displayPlayersName(players)
-      console.log('sup', round)
-
   let clues = new Clues(gameData)
   clues.shuffleCategories()
   clues.pickCategories()
@@ -80,13 +57,7 @@ $('#js-names-button').click(function(e) {
   domUpdates.displayCategories(clues)   
   console.log('sup', round)
   domUpdates.updatePlayerNames()
-
 })
-    });
 
-$('#js-row-8').click(function(e) {
-  e.preventDefault();
-  let clues = new Clues(gameData)
-})
 
 
